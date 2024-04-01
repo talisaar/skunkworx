c0fc065627207facc674794b5f8a17859fa386b1
talisaar <tali.saar@gmail.com>
2024-04-01 09:05:34 +0300

    cleaned up static and now everything is hopefully only in assets

--- tali/tali/settings.py
+++ tali/tali/settings.py
@@ -129,3 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "assets"),
+)
+
