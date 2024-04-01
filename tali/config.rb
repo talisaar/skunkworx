7c073eca3bac8ffa5779bf13b1edca35ebd6e25b
talisaar <tali.saar@gmail.com>
2024-04-01 09:23:49 +0300

    installed compass and added a test scss file with some stuffs

---
+++ tali/config.rb
@@ -0,0 +1,19 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+http_path = "/"
+css_dir = "assets/css"
+sass_dir = "scss"
+images_dir = "assets/img"
+javascripts_dir = "assets/js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+line_comments = true
+
+preferred_syntax = :scss
